84ce1de60b133bf5d0a473df9d197d5da2d4f2bc
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-06-27 15:38:29 +0500

    fix serch image

--- frontend/app/search/image/route.js
+++ frontend/app/search/image/route.js
@@ -3,79 +3,84 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const query = decodeURIComponent(searchParams.get("q")) || "Nothing";
-  const show = decodeURIComponent(searchParams.get("msg"));
-  if (show) {
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            fontSize: 40,
-            color: "white",
-            background: "linear-gradient(135deg, #00a, #002, #000000)",
-            backgroundSize: "100% 100%",
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "40px",
-            fontWeight: "bold",
-            textShadow: "2px 2px 5px black",
-          }}
-        >
-          <p>
-            {show}
-          </p>
-        </div>
-      ),
-      {
-        width: 600,
-        height: 350,
-      }
-    );
-  }
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 40,
-          color: "white",
-          background: "linear-gradient(135deg, #00a, #002, #000000)",
-          backgroundSize: "100% 100%",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "40px",
-          fontWeight: "bold",
-          textShadow: "2px 2px 5px black",
-        }}
-      >
-        <p>
-          {Math.floor(Math.random() * Math.random() * 100)} search results for:{" "}
-          {query}
-        </p>
-        <p
-          style={{
-            fontSize: 16,
-            color: "cyan",
-            textAlign: "center",
-            marginTop: 16,
-            textShadow: "2px 2px 5px black",
-          }}
-        >
-          https://somps.vercel.app
-        </p>
-      </div>
-    ),
-    {
-      width: 600,
-      height: 350,
+  try {
+    const { searchParams } = new URL(req.url);
+    let query = searchParams.get("q") || "Nothing";
+    let show = searchParams.get("msg");
+    if (show) {
+      show = decodeURIComponent(show);
+      return new ImageResponse(
+        (
+          <div
+            style={{
+              fontSize: 40,
+              color: "white",
+              background: "linear-gradient(135deg, #00a, #002, #000000)",
+              backgroundSize: "100% 100%",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "40px",
+              fontWeight: "bold",
+              textShadow: "2px 2px 5px black",
+            }}
+          >
+            <p>{show}</p>
+          </div>
+        ),
+        {
+          width: 600,
+          height: 350,
+        }
+      );
+    } else {
+      query = decodeURIComponent(query);
+      return new ImageResponse(
+        (
+          <div
+            style={{
+              fontSize: 40,
+              color: "white",
+              background: "linear-gradient(135deg, #00a, #002, #000000)",
+              backgroundSize: "100% 100%",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "40px",
+              fontWeight: "bold",
+              textShadow: "2px 2px 5px black",
+            }}
+          >
+            <p>
+              {Math.floor(Math.random() * Math.random() * 100)} search results
+              for: {query}
+            </p>
+            <p
+              style={{
+                fontSize: 16,
+                color: "cyan",
+                textAlign: "center",
+                marginTop: 16,
+                textShadow: "2px 2px 5px black",
+              }}
+            >
+              https://somps.vercel.app
+            </p>
+          </div>
+        ),
+        {
+          width: 600,
+          height: 350,
+        }
+      );
     }
-  );
+  } catch (e) {
+    console.error(e);
+  }
 }
